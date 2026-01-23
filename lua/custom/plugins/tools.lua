2b6553efd67f0807cae58b365562876bf33348d7
FraI3mega <projeapp2@gmail.com>
2026-01-23 19:44:59 +0100

    plugins

---
+++ lua/custom/plugins/tools.lua
@@ -0,0 +1,59 @@
+return {
+	{
+		"NeogitOrg/neogit",
+		lazy = true,
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+
+			-- Only one of these is needed.
+			"nvim-telescope/telescope.nvim", -- optional
+		},
+		cmd = "Neogit",
+		keys = {
+			{ "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+		},
+		opts = {
+			graph_style = vim.env.TERM == "xterm-kitty" and "kitty" or "unicode",
+			disable_signs = true,
+		},
+	},
+	{
+		"3rd/image.nvim",
+		opts = {},
+	},
+	{
+		"MagicDuck/grug-far.nvim",
+		-- Note (lazy loading): grug-far.lua defers all it's requires so it's lazy by default
+		-- additional lazy config to defer loading is not really needed...
+		config = function()
+			-- optional setup call to override plugin options
+			-- alternatively you can set options with vim.g.grug_far = { ... }
+			require("grug-far").setup({
+				-- options, see Configuration section below
+				-- there are no required options atm
+			})
+		end,
+	},
+	{
+		"okuuva/auto-save.nvim",
+		version = "^1.0.0", -- see https://devhints.io/semver, alternatively use '*' to use the latest tagged release
+		cmd = "ASToggle", -- optional for lazy loading on command
+		event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+		opts = {
+			-- your config goes here
+			-- or just leave it empty :)
+			enabled = vim.env.KITTY_SCROLLBACK_NVIM ~= "true",
+			condition = function(buf)
+				local fn = vim.fn
+				local utils = require("auto-save.utils.data")
+
+				-- don't save for `sql` file types
+				if utils.not_in(fn.getbufvar(buf, "&filetype"), { "oil" }) then
+					return true
+				end
+				return false
+			end,
+		},
+	},
+}
