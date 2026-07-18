318c6f5f05977351e9d321d8e300ad580b65899b
FraI3mega <projeapp2@gmail.com>
2026-07-19 00:02:50 +0200

    add auto workspace diag

--- lua/custom/lsp.lua
+++ lua/custom/lsp.lua
@@ -8,6 +8,17 @@ vim.lsp.config("*", {
   end,
 })
 
+vim.lsp.config("*", {
+  on_attach = function(client, bufnr)
+    -- some clients support workspace diagnostics natively
+    if client:supports_method("workspace/diagnostic", bufnr) then
+      vim.lsp.buf.workspace_diagnostics({ client_id = client.id })
+    else
+      require("workspace-diagnostics").populate_workspace_diagnostics(client, bufnr)
+    end
+  end,
+})
+
 vim.lsp.enable("lua_ls")
 vim.lsp.enable("nixd")
 -- vim.lsp.enable("rust_analyzer")
