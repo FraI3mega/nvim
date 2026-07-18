b295b152de4aad1663a148ac1c6e2ac7276e4a55
FraI3mega <projeapp2@gmail.com>
2026-07-19 00:40:03 +0200

    add smart splits

--- lua/custom/plugins/tools.lua
+++ lua/custom/plugins/tools.lua
@@ -1,4 +1,65 @@
 return {
+  {
+    "mrjones2014/smart-splits.nvim",
+    build = "./kitty/install-kittens.bash",
+    event = "VeryLazy",
+    keys = {
+      {
+        "<A-h>",
+        function()
+          require("smart-splits").resize_left()
+        end,
+      },
+      {
+        "<A-j>",
+        function()
+          require("smart-splits").resize_down()
+        end,
+      },
+      {
+        "<A-k>",
+        function()
+          require("smart-splits").resize_up()
+        end,
+      },
+      {
+        "<A-l>",
+        function()
+          require("smart-splits").resize_right()
+        end,
+      },
+      {
+        "<C-h>",
+        function()
+          require("smart-splits").move_cursor_left()
+        end,
+      },
+      {
+        "<C-j>",
+        function()
+          require("smart-splits").move_cursor_down()
+        end,
+      },
+      {
+        "<C-k>",
+        function()
+          require("smart-splits").move_cursor_up()
+        end,
+      },
+      {
+        "<C-l>",
+        function()
+          require("smart-splits").move_cursor_right()
+        end,
+      },
+      {
+        "<C-\\>",
+        function()
+          require("smart-splits").move_cursor_previous()
+        end,
+      },
+    },
+  },
   { "wakatime/vim-wakatime", lazy = false },
   {
     "windwp/nvim-autopairs",
